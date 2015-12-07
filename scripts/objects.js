d54e4a3c1e8f7cfcece73988760f5b7cb43fd563
Stephen Schroeder <schrode50@gmail.com>
2015-12-07 15:29:00 -0800

    all objects done

--- scripts/objects.js
+++ scripts/objects.js
@@ -37,42 +37,142 @@ var pike = {
 pike.totals();
 pike.passData();
 
-
-/*
 var seaTac = {
-  min: 6,
   min: 6,
   max: 44,
   avg: 1.2,
+  hourly: [],
+  total: 0,
   randomHourlyCustomers: function() {
-    return Math.floor(Math.random()*(this.max - this.min + 1)) + this.min);
+    return Math.floor(Math.random()*(this.max - this.min + 1)) + this.min;
+  },
+  hourlySales: function() {
+    return Math.floor(this.avg) * (this.randomHourlyCustomers());
+  },
+  totals: function() {
+    for(i = 0; i < time.length; i++) {
+      var oneHour = this.hourlySales();
+      this.hourly.push(oneHour);
+      this.total += oneHour;
+    }
+  },
+  passData: function() {
+    for(i = 0; i < seaTac.hourly.length; i++) {
+    var node = document.createElement('li');
+    var textNode = document.createTextNode(time[i] + ': ' + this.hourly[i]);
+    node.appendChild(textNode);
+    document.getElementById('seaTac').appendChild(node);
+    }
+    var node = document.createElement('li');
+    var textNode = document.createTextNode('Total: ' + this.total);
+    node.appendChild(textNode);
+    document.getElementById('seaTac').appendChild(node);
   }
 };
+seaTac.totals();
+seaTac.passData();
 
 var southcenter = {
   min: 11,
   max: 38,
   avg: 1.9,
+  hourly: [],
+  total: 0,
   randomHourlyCustomers: function() {
-    return Math.floor(Math.random()*(this.max - this.min + 1)) + this.min);
+    return Math.floor(Math.random()*(this.max - this.min + 1)) + this.min;
+  },
+  hourlySales: function() {
+    return Math.floor(this.avg) * (this.randomHourlyCustomers());
+  },
+  totals: function() {
+    for(i = 0; i < time.length; i++) {
+      var oneHour = this.hourlySales();
+      this.hourly.push(oneHour);
+      this.total += oneHour;
+    }
+  },
+  passData: function() {
+    for(i = 0; i < southcenter.hourly.length; i++) {
+      var node = document.createElement('li');
+      var textNode = document.createTextNode(time[i] + ': ' + this.hourly[i]);
+      node.appendChild(textNode);
+      document.getElementById('southcenter').appendChild(node);
+    }
+    var node = document.createElement('li');
+    var textNode = document.createTextNode('Total: ' + this.total);
+    node.appendChild(textNode);
+    document.getElementById('southcenter').appendChild(node);
   }
 };
+southcenter.totals();
+southcenter.passData();
 
 var bellevue = {
   min: 20,
   max: 48,
-  avg: 3.3
+  avg: 3.3,
+  hourly: [],
+  total: 0,
   randomHourlyCustomers: function() {
-    return Math.floor(Math.random()*(this.max - this.min + 1)) + this.min);
+    return Math.floor(Math.random()*(this.max - this.min + 1)) + this.min;
+  },
+  hourlySales: function() {
+    return Math.floor(this.avg) * (this.randomHourlyCustomers());
+  },
+  totals: function() {
+    for(i = 0; i < time.length; i++) {
+      var oneHour = this.hourlySales();
+      this.hourly.push(oneHour);
+      this.total += oneHour;
+    }
+  },
+  passData: function() {
+    for(i = 0; i < bellevue.hourly.length; i++) {
+      var node = document.createElement('li');
+      var textNode = document.createTextNode(time[i] + ': ' + this.hourly[i]);
+      node.appendChild(textNode);
+      document.getElementById('bellevue').appendChild(node);
+    }
+    var node = document.createElement('li');
+    var textNode = document.createTextNode('Total: ' + this.total);
+    node.appendChild(textNode);
+    document.getElementById('bellevue').appendChild(node);
   }
 };
+bellevue.totals();
+bellevue.passData();
 
 var alki = {
   min: 3,
   max: 24,
   avg: 2.6,
+  hourly: [],
+  total: 0,
   randomHourlyCustomers: function() {
-    return Math.floor(Math.random()*(this.max - this.min + 1)) + this.min);
+    return Math.floor(Math.random()*(this.max - this.min + 1)) + this.min;
+  },
+  hourlySales: function() {
+    return Math.floor(this.avg) * (this.randomHourlyCustomers());
+  },
+  totals: function() {
+    for(i = 0; i < time.length; i++) {
+      var oneHour = this.hourlySales();
+      this.hourly.push(oneHour);
+      this.total += oneHour;
+    }
+  },
+  passData: function() {
+    for(i = 0; i < alki.hourly.length; i++) {
+      var node = document.createElement('li');
+      var textNode = document.createTextNode(time[i] + ': ' + this.hourly[i]);
+      node.appendChild(textNode);
+      document.getElementById('alki').appendChild(node);
+    }
+    var node = document.createElement('li');
+    var textNode = document.createTextNode('Total: ' + this.total);
+    node.appendChild(textNode);
+    document.getElementById('alki').appendChild(node);
   }
-}
-*/
+};
+alki.totals();
+alki.passData();
